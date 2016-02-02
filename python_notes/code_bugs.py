c718a33cb480779f5a225675b2f8e659330e2e30
wl <wenlong@dhcp-10-202-100-224.tamulink.tamu.edu>
2016-02-02 15:28:58 -0600

    iPython Introduction Notes
    Followed
    Python for Data Analysis (By: Wes McKinney): Chapter 3.

---
+++ python_notes/code_bugs.py
@@ -0,0 +1,12 @@
+
+class apple():
+    def __init__(self):
+        self.color = 'green'
+        self.weight = '0.5 pounds'
+    def bug(self,i):
+        if i==0:
+            print 'Good apple!'
+        else:
+            print 'There is a bug in this apple'
+            what a big bug!
+
